5b7d9a2bc7fdc0197fcb62d3a4edc7c965c5fff6
Thomas Coradetti <tmcoradetti@gmail.com>
2020-08-05 20:17:49 -0400

    added write fs

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-let notesArray = [];
 
 //Routes
 app.get("/notes", function (req, res) {
@@ -19,31 +19,48 @@ app.get("/styles", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/css/styles.css"));
 });
 
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.post("/api/notes", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let noteId = (savedNotes.length).toString();
+    newNote.id = noteId;
+    savedNotes.push(newNote);
 
-app.get("/api/notes", function(req, res) {
-    res.json(notesArray);
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
 });
 
-app.post("/api/journals", function (req, res) {
-    notesArray.push(req.body);
-    res.json("saved");
-});
+app.delete("/api/notes/:id", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = req.params.id;
+    let newId = 0;
 
-app.delete("/api/notes/:index", function (req, res) {
-    const elem = parseInt(req.params.index);
-    let tempNote = [];
-    for (let i = 0; i < notesArray.length; i++) {
-        if (i !== elem) {
-            tempNote.push(notesArray[i]);
-        }
+    savedNotes = savedNotes.filter(currentNote => {
+        return currentNote.id != noteId;
+    });
+
+    for (currentNote of savedNotes) {
+        currentNote.id = newId.toString();
+        newId++;
     }
-    notesArray = tempNote;
 
-    res.json("delete done");
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+
+    res.json(savedNotes);
+
 });
 
 //Listener
